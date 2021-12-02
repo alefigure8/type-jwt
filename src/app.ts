01e5058dfc7a1cb1414f7012dc53ad043107249b
ale_figure8 <gomeznieto@gmail.com>
2021-12-02 07:36:57 -0300

    Init

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express'
+import auth from './routes/auth'
+import morgan from 'morgan'
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hola mundo')
-})
+// port
+app.set('port', process.env.PORT || 3000);
+
+// middleware
+app.use(morgan('dev'))
+app.use(express.json())
+
+// routes
+app.use('/', auth)
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from './app'
-app.set('port', process.env.PORT || 3000);
+import './database'
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
 });
